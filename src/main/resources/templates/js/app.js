d444c4a227b3013d459cc0128d291d09b4097aff
Eslam Foad <eslam.foad@gmail.com>
2015-07-05 09:39:34 +0200

    still couldn't server static javascript files

---
+++ src/main/resources/templates/js/app.js
@@ -0,0 +1,31 @@
+// name our angular app
+angular.module('weatherApp', [])
+
+.controller('userController', function() {
+
+	// bind this to vm (view-model)
+  var vm = this;	
+
+
+	// information that comes from our form
+  vm.userData = {};
+  
+  vm.users = [
+     {name: 'Eslam', password: 'sds', mobile: '05545454', email:'esd@f.com'}
+  ];
+
+  vm.register = function() {
+      
+      // add a computer to the list
+      vm.users.push({
+          name: vm.userData.name,
+          email: vm.userDate.email,
+          password: vm.userData.password,
+          mobile: vm.userData.mobile
+      });
+
+      // after our computer has been added, clear the form
+      vm.userData = {};
+  };
+
+});
