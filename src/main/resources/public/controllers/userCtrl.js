a6bd4dac4436b9240e1f27ca305653ce72b8652d
Eslam Foad <eslam.foad@gmail.com>
2015-07-26 09:42:55 +0200

    display login/ regstration erorrs

--- src/main/resources/public/controllers/userCtrl.js
+++ src/main/resources/public/controllers/userCtrl.js
@@ -2,25 +2,33 @@ angular.module('userCtrl', ['userService', 'authService'])
 
   .controller('userController', function($rootScope, $location, User, Auth, AuthToken){
     var vm = this;
-//    vm.userData = {};
+    $rootScope._ = _;
+    vm.loginErrors = {};
+    vm.registrationErrors = {};
     vm.login = function(){
       Auth.login(vm.loginData.email, vm.loginData.password).success(function(data){
-        if (data.erros && data.errors.length > 0){
-          vm.errors = data.errors	
+        if (data.errors && typeof data.errors === 'object'){
+          vm.loginErrors = data.errors	
 //          vm.userData = {};
         }else{
         	$location.path('/');
-        	vm.errors = '';
+        	vm.loginErrors = {};
         	
 //          vm.error = data.message;
         }
       });
     }
+    vm.disable = true;
+    vm.isLoginEnabled = function(){
+    	return vm.loginData.email.length > 0 && vm.loginData.password.length > 0
+    };
     vm.register = function(){
       User.create(vm.userData).success(function(data){
-    	console.log(data);
-        if (data.errors && data.errors.length > 0){
+//    	console.log(data);
+        if (data.errors && typeof data.errors === 'object'){
+        	vm.registrationErrors = data.errors;
         }else{
+        	vm.registrationErrors = {};
         	AuthToken.setToken(data.token);
         	$location.path('/');
         	vm.userData = {};
